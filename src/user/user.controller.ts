844142788c129e89968704f7f61bf1877e90951b
Monday1997 <axu1997@qq.com>
2025-03-24 00:27:36 +0800

    feat: 添加日志插件winston和winston-daily

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, LoggerService } from '@nestjs/common';
 import { UserService2 } from './user.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Controller()
 export class UserController {
-  constructor(private UserService2: UserService2) {
-    console.log(
-      '🚀 ~ UserController ~ constructor ~ UserService2:',
-      UserService2,
-    );
+  constructor(
+    private UserService2: UserService2,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
+  ) {
+    this.logger.log('this is log');
+    this.logger.warn('this is warn');
+    this.logger.error('this is error');
+    this.logger.verbose('this is verbose');
   }
   @Get()
   getUser(): any {
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,64 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService, UserService2 } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 
+function createDailyRotateTransport(level: string, filename: string) {
+  return new winston.transports.DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    WinstonModule.forRoot({
+      level: 'silly', //日志等级 官网查看
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+        // warn级别以上的都在error**.log中
+        createDailyRotateTransport('warn', 'error'),
+        createDailyRotateTransport('info', 'app'),
+        // 使用daily之后就不用winston原生的了
+        // other transports...
+        // new winston.transports.File({
+        //   filename: 'user.log',
+        //   level: 'error',
+        //   format: winston.format.combine(
+        //     winston.format.timestamp(),
+        //     nestWinstonModuleUtilities.format.nestLike('MyApp', {
+        //       prettyPrint: true,
+        //     }),
+        //   ),
+        // }),
+      ],
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, UserService2],
 })
